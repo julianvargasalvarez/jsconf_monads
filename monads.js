1d8de8f4d49ceaddabed0231b583ecb49dd75fbe
Julian Vargas <julianvargasalvarez@gmail.com>
2013-10-18 10:55:23 -0500

    bind"

--- monads.js
+++ monads.js
@@ -12,18 +12,31 @@ var compose = function(f, g){
     return f(g(x));
   }
 }
-var composeDebug = function(f, g) {
-  return function(x) {
-    // x = 3
-    var gx = g(x), // [27, 'cube was called']
-        y = gx[0], // 27
-        s = gx[1], // 'cube was called'
-        fx = f(y), // f(27) -> [0.9, 'sine was called']
-        z = fx[0], // 0.9
-        t = fx[1]; // 'sine was called'
-    return [z, s+', '+t];// [0.9, 'cube was called, sine was called']
+
+//bind (number,string) -> (number,string)
+var bind = function(f) {
+  return function(tuple){
+    var x=tuple[0],
+        s=tuple[1],
+        fx = f(x),
+        y = fx[0],
+        t = fx[1];
+    return [y, s+t];
   }
 }
 
-var sineCubed = composeDebug(sine, cube);
-console.log(sineCubed(3));
+var sineCubed = compose(bind(sine), bind(cube));
+console.log(sineCubed([3, '']));
+
+
+
+
+
+
+
+
+
+
+
+
+
